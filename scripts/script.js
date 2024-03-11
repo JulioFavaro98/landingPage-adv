03b7230780a8da54d01cd8b7d6ba401c0f370037
Julio Favaro <juliofv66@gmail.com>
2024-03-10 21:51:24 -0300

    Primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // Adiciona animação de rolagem suave para os links do menu
+    $(".menu a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
